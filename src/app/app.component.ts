aee86613f342a7f4d0c74573ae216a87407a55b4
Chirag Sane <chiragsane333@gmail.com>
2017-09-25 02:03:11 +0530

    hidden menu when not logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  User: User = {
+    exists: false,
+    name: '',
+    id: ''
+  };
+  ngOnInit(): void {
+  }
+}
+export interface User {
+  exists: boolean;
+  name: string;
+  id: string;
 }
